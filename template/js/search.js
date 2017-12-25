58b7089ff959ab5c43721adfb1285c1f6d264fa8
Ales Tsurko <ales.tsurko@gmail.com>
2017-12-25 15:25:06 +0300

    sort search results by relevance, highlight matches in search results

--- template/js/search.js
+++ template/js/search.js
@@ -6,11 +6,17 @@ DocioSearch.prototypesKeys = Object.keys(DocioSearch.docsData).sort();
 
 DocioSearch.query = ""
 DocioSearch.result = {};
+DocioSearch.result.protos = {};
+DocioSearch.result.slots = {};
+DocioSearch.result.descs = {};
 
 
 DocioSearch.setQuery = function(query) {
     DocioSearch.query = query;
     DocioSearch.result = {};
+    DocioSearch.result.protos = {};
+    DocioSearch.result.slots = {};
+    DocioSearch.result.descs = {};
 
     if (query.length > 1) {
         DocioSearch.__searchPrototypes();
@@ -29,10 +35,22 @@ DocioSearch.__searchPrototypes = function() {
         var protoKeyMatch = element.match(regex);
         var descMatch = protoDesc.match(regex);
 
-        if ((protoKeyMatch && protoKeyMatch.length > 0) || (descMatch && descMatch.length > 0)) {
-            DocioSearch.result[element] = {};
-            DocioSearch.result[element]["description"] = protoDesc;
-            DocioSearch.result[element]["url"] = DocioSearch.urlPrefix + element.toLowerCase() + ".html";
+        if (protoKeyMatch && protoKeyMatch.length > 0) {
+            // highlight match
+            var key = element.replace(regex, "<span class=\"uk-text-success\">$&</span>");
+            // add match to object
+            DocioSearch.result.protos[key] = {};
+            DocioSearch.result.protos[key]["description"] = protoDesc;
+            DocioSearch.result.protos[key]["url"] = DocioSearch.urlPrefix + element.toLowerCase() + ".html";
+        }
+
+        if (descMatch && descMatch.length > 0 && !DocioSearch.result.protos[element]) {
+            // highlight match
+            protoDesc = protoDesc.replace(regex, "<span class=\"uk-text-success\">$&</span>");
+            // add match to object
+            DocioSearch.result.descs[element] = {};
+            DocioSearch.result.descs[element]["description"] = protoDesc;
+            DocioSearch.result.descs[element]["url"] = DocioSearch.urlPrefix + element.toLowerCase() + ".html";
         }
 
         DocioSearch.__searchSlotsInPrototype(element);
@@ -49,12 +67,31 @@ DocioSearch.__searchSlotsInPrototype = function(prototypeKey) {
         slotDescription = DocioSearch.docsData[prototypeKey].slots[slotKey];
 
         var slotKeyMatch = slotKey.match(regex);
+        var key = prototypeKey + " " + slotKey;
+        // it's possible to include all the slots for matching prototype
+        // by uncomment bellow lines, but does it needed? 
+        // The searching results now is: prototype names (and there is all the slots), 
+        // slot names, matches in descriptions for prototypes and slots.
+        // var slotKeyMatch = key.match(regex);
         var descMatch = slotDescription.match(regex);
 
-        if ((slotKeyMatch && slotKeyMatch.length > 0) || (descMatch && descMatch.length > 0)) {
-            DocioSearch.result[prototypeKey + " " + slotKey] = {};
-            DocioSearch.result[prototypeKey + " " + slotKey]["description"] = slotDescription;
-            DocioSearch.result[prototypeKey + " " + slotKey]["url"] = DocioSearch.urlPrefix + prototypeKey.toLowerCase() + ".html#" + slotKey;
+        if (slotKeyMatch && slotKeyMatch.length > 0) {
+            // highlight match
+            var slotName = slotKey.replace(regex, "<span class=\"uk-text-success\">$&</span>");
+            key = prototypeKey + " " + slotName;
+            // add match to object
+            DocioSearch.result.slots[key] = {};
+            DocioSearch.result.slots[key]["description"] = slotDescription;
+            DocioSearch.result.slots[key]["url"] = DocioSearch.urlPrefix + prototypeKey.toLowerCase() + ".html#" + slotKey;
+        }
+
+        if (descMatch && descMatch.length > 0 && !DocioSearch.result.slots[key]) {
+            // highlight match
+            slotDescription = slotDescription.replace(regex, "<span class=\"uk-text-success\">$&</span>");
+            // add match to object
+            DocioSearch.result.descs[key] = {};
+            DocioSearch.result.descs[key]["description"] = slotDescription;
+            DocioSearch.result.descs[key]["url"] = DocioSearch.urlPrefix + prototypeKey.toLowerCase() + ".html#" + slotKey;
         }
     });
 }
@@ -80,10 +117,7 @@ DocioSearchView.clearResultsList = function() {
 DocioSearchView.redrawResultsList = function() {
     DocioSearchView.clearResultsList();
 
-    Object.keys(DocioSearch.result).forEach(function(resName) {
-        var resDesc = DocioSearch.result[resName]["description"];
-        var url = DocioSearch.result[resName]["url"];
-
+    var addResultEntry = function(resName, resDesc, url) {
         var tr = document.createElement("tr");
 
         // title
@@ -92,8 +126,9 @@ DocioSearchView.redrawResultsList = function() {
         link.setAttribute("href", url);
 
         var tdTitle = document.createElement("td");
-        var title = document.createTextNode(resName);
-        link.appendChild(title);
+        // var title = document.createTextNode(resName);
+        console.log(resName);
+        link.innerHTML = resName;
         tdTitle.appendChild(link);
         tdTitle.setAttribute("class", "uk-width-small uk-table-link");
 
@@ -113,6 +148,30 @@ DocioSearchView.redrawResultsList = function() {
 
         var resultsList = document.getElementById("search-results-table");
         resultsList.appendChild(tr);
+    };
+
+    // Prototypes
+    Object.keys(DocioSearch.result.protos).forEach(function(resName) {
+        var resDesc = DocioSearch.result.protos[resName]["description"];
+        var url = DocioSearch.result.protos[resName]["url"];
+
+        addResultEntry(resName, resDesc, url);
+    });
+
+    // Slots
+    Object.keys(DocioSearch.result.slots).forEach(function(resName) {
+        var resDesc = DocioSearch.result.slots[resName]["description"];
+        var url = DocioSearch.result.slots[resName]["url"];
+
+        addResultEntry(resName, resDesc, url);
+    });
+
+    // Descriptions
+    Object.keys(DocioSearch.result.descs).forEach(function(resName) {
+        var resDesc = DocioSearch.result.descs[resName]["description"];
+        var url = DocioSearch.result.descs[resName]["url"];
+
+        addResultEntry(resName, resDesc, url);
     });
 }
 
